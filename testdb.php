7eb7af5c0dbadc7f1113754d2e032e325f24f717
Ameed Jamous <ssw@joodtelecomgroup.com>
2012-09-25 15:05:53 +0300

    Changed db password

--- testdb.php
+++ testdb.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 print_r($_SESSION);
-$link = mysql_connect("localhost","root","dbpass");
+$link = mysql_connect("localhost","root","dbpassword");
 if (!$link) {
     die('Not connected : ' . mysql_error());
 }
