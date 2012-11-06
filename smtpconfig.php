3fb8a768a749a8e8196a0f7a84cf386a0c1e5d9c
Ameed Jamous <ssw@joodtelecomgroup.com>
2012-11-07 01:41:29 +0200

    Update smtpconfig.php
    small fix.

--- smtpconfig.php
+++ smtpconfig.php
@@ -3,8 +3,8 @@ require("./class.phpmailer.php");
 $mail = new PHPMailer();
 $mail->IsSMTP();                                
 $mail->Host     = "localhost"; 
-//$mail->Host     = "mail.joodtelecomgroup.com"; 
+//$mail->Host     = "locahost"; 
 $mail->SMTPAuth = false;   
-//$mail->Username = "team@joodtelecomgroup.com";  
-//$mail->Password = "fidelpiero";
+//$mail->Username = "none";  
+//$mail->Password = "none";
 ?>
