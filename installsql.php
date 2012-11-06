c555f39ab99aa561e12f4c05d91c483facc62bd1
Ameed Jamous <ssw@joodtelecomgroup.com>
2012-11-07 01:43:18 +0200

    Update installsql.php
    Fix creating userloginstatus table which is responsible in showing the online number of users in admin panel.

--- installsql.php
+++ installsql.php
@@ -15,7 +15,7 @@ $sql = "CREATE TABLE userloginstatus
 (
 id_client varchar(15),
 user_status int
-)";
+);
 mysql_query($sql);
 mysql_query("ALTER TABLE callscallshop ADD paid INT NOT NULL DEFAULT 0;");
 mysql_query("UPDATE callscallshop SET paid=1;");
